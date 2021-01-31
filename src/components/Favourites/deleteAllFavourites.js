93f5e98348f92567a2e060880f91d1cb7663e1da
Alex <alexdanisz@live.com>
2021-01-30 19:19:07 -0500

    Finished delete all modal

--- src/components/Favourites/deleteAllFavourites.js
+++ src/components/Favourites/deleteAllFavourites.js
@@ -1,21 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { deleteAllItems } from "../../redux/favourites/favourite.actions";
 
-import { deleteAllItems} from "../../redux/favourites/favourite.actions";
+// Modal Styles
+//import Button from '@material-ui/core/Button';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { useMediaQuery } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
 
+import "./modal.styles.scss";
 
 const DeleteAllFavourites = () => {
-    const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleButton = () => {
-        dispatch(deleteAllItems())
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    }
+  const handleDeleteAll = () => {
+    dispatch(deleteAllItems());
+  };
 
-    return (
-        <button onClick={handleButton} className="btn favs__btn favs__btn--all">Delete All</button>
-    )
-}
+  return (
+    <>
+      <button
+        onClick={handleClickOpen}
+        className="btn favs__btn favs__btn--all"
+      >
+        Delete All
+      </button>
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <h3 className="modal__title" id="alert-dialog-title">
+          {"Are you sure?"}
+        </h3>
+        <DialogContent>
+          <p className="modal__text" id="alert-dialog-description">
+            This process cannot be undone.
+          </p>
+        </DialogContent>
+        <div className="modal__btnContainer">
+          <button
+            className="btn favs__btn favs__btn--delete"
+            onClick={() => {
+              handleDeleteAll();
+              handleClose();
+            }}
+            color="primary"
+            autoFocus
+          >
+            Delete
+          </button>
+          <button
+            className="btn favs__btn favs__btn--cancel"
+            onClick={handleClose}
+            color="primary"
+          >
+            Cancel
+          </button>
+        </div>
+      </Dialog>
+    </>
+  );
+};
 
-export default DeleteAllFavourites
+export default DeleteAllFavourites;
